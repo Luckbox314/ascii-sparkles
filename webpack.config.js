fdb8c4095aa96eb55da745c92106218719d4e0d2
Luckbox314 <lucas.muoz@uc.cl>
2023-04-25 17:19:16 -0400

    setup

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+// const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: "development",
+  // plugins: [
+  //   new CopyPlugin({
+  //     patterns: [
+  //       {from: 'public'}
+  //     ]
+  //   })
+  // ],
+};
