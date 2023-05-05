d4dd30657f4437dd839f5f280da655866763b58f
Luckbox314 <lucas.muoz@uc.cl>
2023-05-05 00:43:48 -0400

    updated to include dev server

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  mode: "development",
+  mode: "production",
   // plugins: [
   //   new CopyPlugin({
   //     patterns: [
